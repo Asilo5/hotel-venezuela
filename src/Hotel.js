1f429a12194c6841bf8fbdacd61d9e0dd5b04cab
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-09-14 13:12:27 -0600

    Write tests for Hotel class

--- src/Hotel.js
+++ src/Hotel.js
@@ -1,4 +1,50 @@
 class Hotel {
+  constructor(data) {
+    this.users = data.users;
+    this.rooms = data.rooms;
+    this.bookings = data.bookings;
+    this.roomServices = data.roomServices;
+  }
+
+  dailyRoomServiceCharges(date) {
+
+  }
+
+  dailyBookings(date) {
+
+  }
+
+  searchGuestInData(name) {
+    // return ID
+  }
+
+  findExistingGuest(name) {
+    // here we instantiate the new customer
+  }
+
+  roomsAvailableToday(date) {
+
+  }
+
+  revenueToday(date) {
+
+  }
+
+  roomsPercentOccupiedToday(date) {
+
+  }
+
+  popularBookingDate() {
+
+  }
+
+  mostRoomsAvailableDay() {
+
+  }
+
+  filterRoomsByType(type) {
+      
+  }
 
 }
 
--- test/Hotel-test.js
+++ test/Hotel-test.js
@@ -15,4 +15,48 @@ describe('Hotel', () => {
     expect(Hotel).to.be.a('function');
   })
 
+  it('should show all room service charges for that date', () => {
+    expect(hotel.dailyRoomServiceCharges(date)).to.eql();
+  })
+
+  it('should show all bookings for that date', () => {
+    expect(hotel.dailyBookings(date)).to.eql();
+  })
+
+  it('should use the name of the guest to find the ID', () => {
+    expect(hotel.searchGuestInAllData(name)).to.eql();
+  })
+
+  it('should find existing guest', () => {
+    expect(hotel.findExistingGuest(name)).to.eql();
+  })
+
+  it('should add guest to data', () => {
+    expect(hotel.addGuest(name)).to.eql();
+  })
+
+  it('should return rooms available today', () => {
+    expect(hotel.roomsAvailableToday(date)).to.eql();
+  })
+
+  it('should return revenue today', () => {
+    expect(hotel.revenueToday(date)).to.eql();
+  })
+
+  it('should return percent of rooms occupied today', () => {
+    expect(hotel.roomsPercentOccupiedToday(date)).to.eql();
+  })
+
+  it('should return most popular booking date', () => {
+    expect(hotel.popularBookingDate()).to.equal();
+  })
+
+  it('should return date with most rooms available', () => {
+    expect(hotel.mostRoomsAvailableDay()).to.equal();
+  })
+
+  it('should filter rooms by type', () => {
+    expect(hotel.filterRoomsByType(type)).to.equal();
+  })
+
 })
