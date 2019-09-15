6d494618b45d060137003341a498535e617d8f6c
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-09-15 16:18:53 -0600

    Start fetching data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
+console.log('This is the JavaScript entry file - your code begins here.');
 
 import $ from 'jquery';
 import domUpdates from "./domUpdates";
 import './css/base.scss';
+import Hotel from './Hotel';
  
+let guestsData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users');
+let roomsData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms');
+let bookingsData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings');
+let roomServiceData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices'); 
 let hotel;
 
-console.log('This is the JavaScript entry file - your code begins here.');
+Promise.all([guestsData, roomsData, bookingsData, roomServiceData])
+  .then(values => Promise.all(values.map((value) => value.json())))
+  .then(completeData => {
+    let guests = completeData.find((data) => data.hasOwnProperty('users')).users;
+    let rooms = completeData.find((data) => data.hasOwnProperty('rooms')).rooms;
+    let bookings = completeData.find((data) => data.hasOwnProperty('bookings')).bookings;
+    let roomServices = completeData.find((data) => data.hasOwnProperty('roomServices')).roomServices;
+    hotel = new Hotel(guests, rooms, bookings, roomServices);
+  })
+
+console.log(hotel.roomServices);
 
 $('.tab-container .tabs li').on('click', function(e) {
   e.preventDefault();
