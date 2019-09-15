7d10d328613668924d19972e42c43dbd887c5ec3
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-09-15 15:45:17 -0600

    Add current date method

---
+++ src/domUpdates.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+
+let domUpdates = {
+
+  findCurrentDate() {
+    let today = new Date();
+    let day = String(today.getDate()).padStart(2, '0');
+    let month = String(today.getMonth() + 1).padStart(2, '0');
+    let year = today.getFullYear();
+    return `${year}/${month}/${day}`;
+  },
+
+};
+
+export default domUpdates;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@
 
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
+import domUpdates from "./domUpdates";
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-
+ 
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
@@ -25,4 +26,4 @@ $('.tab-container .tabs li').on('click', function(e) {
   });
 });
 
-// let today = domUpdates.findCurrentDate();
+let today = domUpdates.findCurrentDate();
